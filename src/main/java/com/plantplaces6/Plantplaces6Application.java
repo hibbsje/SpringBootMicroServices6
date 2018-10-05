2f207d5e5ce0e953bf7f056c8f49e8c72eaa7158
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-04 20:38:11 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/plantplaces6/Plantplaces6Application.java
@@ -0,0 +1,12 @@
+package com.plantplaces6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plantplaces6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plantplaces6Application.class, args);
+	}
+}
---
+++ src/test/java/com/plantplaces6/plantplaces6/Plantplaces6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.plantplaces6.plantplaces6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Plantplaces6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
