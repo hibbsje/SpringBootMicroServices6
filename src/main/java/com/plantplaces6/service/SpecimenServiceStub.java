172d4826db0a54b28592c4151cdf5c9729ff9e9c
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-10 21:19:29 -0400

    missed java doc

--- src/main/java/com/plantplaces6/service/SpecimenServiceStub.java
+++ src/main/java/com/plantplaces6/service/SpecimenServiceStub.java
@@ -5,14 +5,23 @@ import org.springframework.stereotype.Component;
 import com.plantplaces6.dto.SpecimenDTO;
 
 @Component
-public class SpecimenServiceStub {
+public class SpecimenServiceStub implements ISpecimenService {
 
+	/* (non-Javadoc)
+	 * @see com.plantplaces6.service.ISpecimenService#fetchById(int)
+	 */
+	@Override
 	public SpecimenDTO fetchById(int id) {
 		SpecimenDTO specimenDTO = new SpecimenDTO();
 		specimenDTO.setSpecimenId(43);
 		return specimenDTO;
+		
 	}
 	
+	/* (non-Javadoc)
+	 * @see com.plantplaces6.service.ISpecimenService#save(com.plantplaces6.dto.SpecimenDTO)
+	 */
+	@Override
 	public void save(SpecimenDTO specimenDTO) {
 		
 	}
